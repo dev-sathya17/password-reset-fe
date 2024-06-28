52c2f526a48fcba1aa85dd881e89917f69905575
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-28 12:00:54 +0530

    completed UI with validations and design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import axios from "axios";
 import { useState } from "react";
-
+import "./Page.css";
+import { MdOutlineMail } from "react-icons/md";
 const Home = () => {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = () => {
-    console.log("clicked");
+    if (!email.includes("@") || !email.includes(".com")) {
+      setError("Please enter a valid email");
+      return;
+    }
+
     axios
       .post("https://password-reset-j80k.onrender.com/users/forgot", {
         email,
@@ -21,16 +27,44 @@ const Home = () => {
       });
   };
 
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    if (e.target.value === "") {
+      setError("Email is required");
+    } else {
+      setError("");
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email Address:"
-        />
-        <button onClick={handleSubmit}>SUBMIT</button>
+    <div className="wrapper">
+      <div className="form-container">
+        <h1>Forgot Password</h1>
+        <div className="input-container">
+          <MdOutlineMail className="icon" />
+          <input
+            type="email"
+            value={email}
+            onChange={handleChange}
+            placeholder="abcd@email.com:"
+            id="email"
+            className={error ? "input-error" : ""}
+          />
+        </div>
+        {error ? (
+          <div className="error">
+            <p className="error-message">{error}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+        <button
+          onClick={handleSubmit}
+          className={error ? "button-error" : "button"}
+          disabled={error ? true : false}
+        >
+          SUBMIT
+        </button>
       </div>
     </div>
   );
--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -1,78 +1,173 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { FaLock } from "react-icons/fa";
+import { MdError } from "react-icons/md";
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
 
 const Verify = () => {
   const { authString } = useParams();
 
-  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState();
+  const [error, setError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const hasNumber = /\d/;
+  const hasLowercase = /[a-z]/;
+  const hasUppercase = /[A-Z]/;
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
 
   useEffect(() => {
-    setLoading(true);
     verify();
   }, []);
 
   const verify = async () => {
-    const response = await axios.get(
-      `https://password-reset-j80k.onrender.com/users/verify/${authString}`
-    );
-
-    if (response.status === 200) {
-      setEmail(response.data.email);
-      setShowForm(true);
+    try {
+      const response = await axios.get(
+        `https://password-reset-j80k.onrender.com/users/verify/${authString}`
+      );
+      if (response.status === 200) {
+        setEmail(response.data.email);
+        setShowForm(true);
+      }
+    } catch (error) {
+      setShowForm(false);
     }
-    setLoading(false);
+  };
+
+  const isValidPassword = (password) => {
+    if (password.length < 8 || password.length > 20) {
+      setError("Password must be between 8 and 20 characters long");
+      return false;
+    } else if (!hasNumber.test(password)) {
+      setError("Password must contain at least one number");
+      return false;
+    } else if (!hasLowercase.test(password)) {
+      setError("Password must contain at least one lowercase letter");
+      return false;
+    } else if (!hasUppercase.test(password)) {
+      setError("Password must contain at least one uppercase letter");
+      return false;
+    } else if (!hasSpecialChar.test(password)) {
+      setError("Password must contain at least one special character");
+      return false;
+    } else {
+      setError("");
+      return true;
+    }
+  };
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+    isValidPassword(password);
+  };
+
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+    if (password !== e.target.value) {
+      setConfirmPasswordError("Passwords do not match");
+    } else {
+      setConfirmPasswordError("");
+    }
+  };
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   const handleConfirm = () => {
-    if (password === confirmPassword) {
-      axios
-        .post("https://password-reset-j80k.onrender.com/users/reset", {
-          email,
-          password,
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            alert(response.data.message);
-            setPassword("");
-            setEmail("");
-            setConfirmPassword("");
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } else {
-      alert("Passwords do not match");
-    }
+    axios
+      .post("https://password-reset-j80k.onrender.com/users/reset", {
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          alert(response.data.message);
+          setPassword("");
+          setEmail("");
+          setConfirmPassword("");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
-    <div>
-      {loading ? (
-        <></>
-      ) : showForm ? (
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-          />
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder="Confirm Password"
-          />
+    <div className="wrapper">
+      {showForm ? (
+        <div className="form-container">
+          <h1>Reset Your Password</h1>
+          <div className="input-container">
+            <FaLock className="icon" />
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={handleChange}
+              placeholder="Password"
+              id="password"
+            />
+            {showPassword ? (
+              <IoMdEye className="eye-icon" onClick={togglePassword} />
+            ) : (
+              <IoMdEyeOff className="eye-icon" onClick={togglePassword} />
+            )}
+          </div>
+          {error ? (
+            <div className="error">
+              <p className="error-message">{error}</p>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="input-container">
+            <FaLock className="icon" />
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              value={confirmPassword}
+              onChange={handleConfirmPassword}
+              placeholder="Confirm Password"
+              id="confirm-password"
+            />
+            {showConfirmPassword ? (
+              <IoMdEye className="eye-icon" onClick={toggleConfirmPassword} />
+            ) : (
+              <IoMdEyeOff
+                className="eye-icon"
+                onClick={toggleConfirmPassword}
+              />
+            )}
+          </div>
+          {confirmPasswordError ? (
+            <div className="error">
+              <p className="error-message">{confirmPasswordError}</p>
+            </div>
+          ) : (
+            <></>
+          )}
           <button onClick={handleConfirm}>CONFIRM</button>
         </div>
       ) : (
-        <></>
+        <div className="not-found-wrapper">
+          <div className="error-title">
+            <MdError className="error-icon" />
+            <h1>The Link you have entered is not valid</h1>
+          </div>
+          <div>
+            <h2>Kindly verify the link again!</h2>
+          </div>
+        </div>
       )}
     </div>
   );
